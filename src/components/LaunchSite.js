fdfab3bca31b22ea72b44e173ff3cab9468e7aee
Rafał Skwara <RSkwara@alfa.wsip.com.pl>
2018-05-08 10:12:16 +0200

    minor styling corrections

--- src/components/LaunchSite.js
+++ src/components/LaunchSite.js
@@ -1,31 +1,32 @@
-import React from "react"
-import "../styles/LaunchSite.sass"
+import React from 'react'
+import '../styles/LaunchSite.sass'
 
-export default class LaunchSite extends React.Component {
-    // eslint-disable-line react/prefer-stateless-function
+export default class LaunchSite extends React.Component { // eslint-disable-line react/prefer-stateless-function   
 
     render() {
-        return <div className={"launchsite-block"}>
-                <h3> Launch Site </h3>
-                <div className={"launchsite__column"}>
-                    <div className={"subcolumn--left"}>Name:</div>
-                    <div className={"subcolumn--right"}>
-                        {this.props.launchSite["full_name"]}
-                    </div>
+        // data extracted from JSON in props:
+        const fullName = this.props.launchSite['full_name'];
+        const name = this.props.launchSite.location.name;
+        const region = this.props.launchSite.location.region;
+        const details = this.props.launchSite.details;
+
+        return (<div className={'launchsite-block'}>
+            <h3> Launch Site </h3>
+            <div className={'launchsite__column'}>
+                <div className={'subcolumn--left'}>Name:</div>
+                <div className={'subcolumn--right'}>
+                    {fullName}
                 </div>
-                <div className={"launchsite__column"}>
-                    <div className={"subcolumn--left"}>Location:</div>
-                    <div className={"subcolumn--right"}>
-                        {this.props.launchSite.location.name}, <br />
-                        {this.props.launchSite.location.region}
-                    </div>
-                </div>
-                <p
-                    className={
-                        "launchsite__additional-info launchsite-block__par"
-                    }
-                > {this.props.launchSite.details}
-                </p>
             </div>
+            <div className={'launchsite__column'}>
+                <div className={'subcolumn--left'}>Location:</div>
+                <div className={'subcolumn--right'}>
+                    {name}, <br />
+                    {region}
+                </div>
+            </div>
+            <p className={'launchsite__additional-info launchsite-block__par'}> {details}
+            </p>
+        </div>)
     }
 }
