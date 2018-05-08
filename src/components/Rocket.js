3306198b6943202781e8efd4187a9e91d88dfe38
Rafał Skwara <RSkwara@alfa.wsip.com.pl>
2018-05-08 08:29:19 +0200

    main works on details component done

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,75 +1,91 @@
 import React from "react"
 import "../styles/Rocket.sass"
 
-export default class Rocket extends React.Component {
-                   // eslint-disable-line react/prefer-stateless-function
+export default class Rocket extends React.Component { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return <div className={"rocket-block"}>
+                <h3> Rocket </h3>
+                <div className={"rocket__column"}>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>Name:</div>
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.name}
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            Company:
+                        </div>
 
-                   render() {
-                       return <div className={"rocket-block"}>
-                               <h3> Rocket </h3>
-       
-           
-                               <div className={"rocket__column"}>
-                                    <div className={"rocket__row"}>
-                                        <div className={"subcolumn--left"}>
-                                            Name:
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.company}
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>Height:</div>
 
-                                        </div>
-                                    
-                                        <div className={"subcolumn--right"}>
-                                            {this.props.rocket.name}
-                                        </div>
-                                    </div>
-                                    <div className={"rocket__row"}>
-                                        <div className={"subcolumn--left"}>
-                                            Company:
-                                        </div>
-                                    
-                                        <div className={"subcolumn--right"}>
-                                            {this.props.rocket.company}
-                                        </div>
-                                    </div>
-                                    <div className={"rocket__row"}>
-                                        <div className={"subcolumn--left"}>
-                                            Height:
-                                        </div>
-                                    
-                                        <div className={"subcolumn--right"}>
-                                            {this.props.rocket.height.meters}M / {this.props.rocket.height.feet}FT
-                                        </div>
-                                    </div>
-                                     <div className={"rocket__row"}>
-                                        <div className={"subcolumn--left"}>
-                                            Diameter:
-                                        </div>
-                                    
-                                        <div className={"subcolumn--right"}>
-                                            {this.props.rocket.diameter.meters}M / {this.props.rocket.diameter.feet}FT
-                                        </div>
-                                    </div>
-                                     <div className={"rocket__row"}>
-                                        <div className={"subcolumn--left"}>
-                                            Mass:
-                                        </div>
-                                    
-                                        <div className={"subcolumn--right"}>
-                                            {this.props.rocket.mass.kg}KG / {this.props.rocket.mass.lb}Lb
-                                        </div>
-                                    </div>
-                               </div>
-                               <div className={"rocket__column"}>
-                               <div className={"rocket__row"}>
-                                   <div className={"subcolumn--left" } >
-                                       First flight:
-                                   </div>
-                                   <div className={"subcolumn--right"}>
-                                       {this.props.rocket["first_flight"]}
-                                   </div>
-                                    </div>
-                               </div>
-                               <p className={ "rocket__additional-info rocket-block__par" } >
-                                   { this.props.rocket.description }
-                               </p>
-                           </div>
-                   }
-               }
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.height.meters}M / {this.props.rocket.height.feet}FT
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            Diameter:
+                        </div>
+
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.diameter.meters}M / {this.props.rocket.diameter.feet}FT
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>Mass:</div>
+
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.mass.kg}KG / {this.props.rocket.mass.lb}Lb
+                        </div>
+                    </div>
+                </div>
+                <div className={"rocket__column"}>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            First flight:
+                        </div>
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket["first_flight"]}
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            Country:
+                        </div>
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket.country}
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            Success rate:
+                        </div>
+                        <div className={"subcolumn--right"}>
+                            {this.props.rocket["success_rate_pct"]}%
+                        </div>
+                    </div>
+                    <div className={"rocket__row"}>
+                        <div className={"subcolumn--left"}>
+                            Cost per launch:
+                        </div>
+                        <div className={"subcolumn--right"}>
+                            ${this.props.rocket["cost_per_launch"]}
+                        </div>
+                    </div>
+                </div>
+                <p
+                    className={
+                        "rocket__additional-info rocket-block__par"
+                    }
+                >
+                    {this.props.rocket.description}
+                </p>
+            </div>
+    }
+}
